bf766c57f4f7f4f7a2d1713997ae1b6c759fe0b8
ssamuel-droid <ssamuel@goodhealthcorp.com>
2026-08-27 14:54:13 -0700

    Speed up Railway builds

--- src/pdf/DealMemo.js
+++ src/pdf/DealMemo.js
@@ -431,6 +431,7 @@ async function generateDealMemo(siteModel, options = {}) {
 
   const browser = await puppeteer.launch({
     headless: 'new',
+    executablePath: process.env.PUPPETEER_EXECUTABLE_PATH || undefined,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
 
